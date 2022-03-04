0f7bbb3d6f5d67e53eee8f53abd2dd235679ffcc
IsseiMori <ganntyu1225issei@gmail.com>
2022-03-04 13:43:04 -0800

    update gitignore

--- data_uploader.bat
+++ data_uploader.bat
@@ -3,11 +3,12 @@ rem kubectl cp tmp/FLEX_RiceGrip imori-pod-file-transfer:imori-fast-vol/Neural_S
 rem kubectl cp tmp/FLEX_RiceGrip/predicted_data imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip
 rem kubectl cp tmp/FLEX_RiceGrip/mpm_data imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip
 
-
+rem kubectl cp tmp/FLEX_RiceGrip/data/metadata.json imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/data
+rem kubectl cp tmp/FLEX_RiceGrip/data/rollouts/metadata.json imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/data/rollouts
 
 rem Download
-rem kubectl cp imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/rollouts/test_500000 tmp/FLEX_RiceGrip/rollouts/test_500000
+kubectl cp imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/rollouts/test_300000 tmp/FLEX_RiceGrip/rollouts/test_300000
+kubectl cp imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/models/model_300000.pth tmp/FLEX_RiceGrip/models/model_300000.pth
 
-kubectl cp imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/models/model_500000.pth tmp/FLEX_RiceGrip/models/model_500000.pth
-kubectl cp imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/mpm_data tmp/FLEX_RiceGrip/mpm_data
-kubectl cp imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/predicted_data tmp/FLEX_RiceGrip/predicted_data
+rem kubectl cp imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/mpm_data tmp/FLEX_RiceGrip/mpm_data
+rem kubectl cp imori-pod-file-transfer:imori-fast-vol/Neural_Simulation/tmp/FLEX_RiceGrip/predicted_data tmp/FLEX_RiceGrip/predicted_data
