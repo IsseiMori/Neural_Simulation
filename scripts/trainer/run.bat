1b0ee0b7a6d136c0c0edad4b13a5be2de0f254ff
IsseiMori <ganntyu1225issei@gmail.com>
2022-03-23 16:01:26 -0700

    heat map

---
+++ scripts/trainer/run.bat
@@ -0,0 +1,13 @@
+python inference.py \
+    --data_path=../../tmp/Grid/RiceGrip/FLEX/data \
+    --metadata_path=../../tmp/Finetune/RiceGrip/FLEX/full/data \
+    --model=../../tmp/Finetune/RiceGrip/FLEX/full/models/model_335000.pth \
+    --output_path=../../tmp/Finetune/RiceGrip/FLEX/full/grid  \
+    --dim=6
+
+python inference.py \
+    --data_path=../../tmp/Grid/RiceGrip/FLEX/data \
+    --metadata_path=../../tmp/Finetune/RiceGrip/FLEX/partial/data \
+    --model=../../tmp/Finetune/RiceGrip/FLEX/partial/models/model_370000.pth \
+    --output_path=../../tmp/Finetune/RiceGrip/FLEX/partial/grid  \
+    --dim=6
